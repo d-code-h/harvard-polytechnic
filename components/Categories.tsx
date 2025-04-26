9af9f690f573b11643451a49eea1536b8f655a95
Yunus Habeeb <habeebdh1@outlook.com>
2025-04-26 10:29:43 +0100

    Refactor Categories component layout and styles; update heading and button styles for improved consistency and responsiveness

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -5,22 +5,22 @@ import { courseCategories } from '@/lib/data';
 
 const Categories = () => {
   return (
-    <div className="mb-12">
-      <h2 className="text-dark font-bold text-5xl text-center mt-8 mb-16">
+    <div className="mb-8">
+      <h2 className="text-dark font-epilogue font-bold text-5xl text-center mt-16 mb-12">
         Browse By Categories
       </h2>
 
-      <div className="flex flex-1 flex-wrap items-center justify-center gap-6 px-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-5 md:px-14">
         {courseCategories.map((category) => (
           <Button
             key={category.text}
-            className={`flex items-center justify-start gap-3 md:gap-5 rounded-xl px-5 py-6 bg-[${category.color}] w-full sm:w-[380px] h-auto`}
+            className={`flex items-center justify-start gap-3 md:gap-5 rounded-xl px-3 py-4 md:px-5 md:py-6 bg-[${category.color}]  h-auto`}
             style={{
               backgroundColor: category.color + '30',
             }}
           >
             <div
-              className={`p-5 rounded-full border border-[${category.color}]`}
+              className={`p-3 lg:p-5 rounded-full border border-[${category.color}]`}
               style={{
                 backgroundColor: category.color + '2A',
                 border: '2px solid ' + category.color,
@@ -34,7 +34,7 @@ const Categories = () => {
                 className="w-10 h-10"
               />
             </div>
-            <span className="text-dark text-base font-semibold">
+            <span className="text-dark font-epilogue font-bold text-base lg:text-xl whitespace-break-spaces">
               {category.text}
             </span>
           </Button>
